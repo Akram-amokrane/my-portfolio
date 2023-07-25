9b886755a9fe1b1ed6f3e90628a440c39dc94570
MIS <amokraneakram54@gmail.com>
2023-07-25 19:28:29 +0100

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,25 @@
-"use client"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import { register } from "swiper/element/bundle";
 
-import './globals.css'
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import {register} from "swiper/element/bundle"
+register();
 
-register()
-
-const inter = Montserrat({ subsets: ['latin'] })
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Akram Amokrane',
-  description: 'My Portfolio',
-}
+  title: "Akram Amokrane",
+  description: "My Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" >
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
