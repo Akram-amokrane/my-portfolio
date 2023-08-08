16c139f6b2cc4e1d47e49a4988e7543ec9370ec2
MIS <amokraneakram54@gmail.com>
2023-08-08 19:19:45 +0100

    fix light as default

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -7,5 +7,9 @@ export default function MyThemeProvider({
 }: {
   children: React.ReactNode;
 }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      {children}
+    </ThemeProvider>
+  );
 }
